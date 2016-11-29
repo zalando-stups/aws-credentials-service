9a57dfcdf2c36edd5ef60559d666efac98f9e50c
Henning Jacobs <henning@jacobs1.de>
2016-11-29 19:22:07 +0100

    check LDAP group

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
 import logging
+import re
 
 import connexion
+import requests
+
+GROUPS_URL = 'https://users.auth.zalando.com/employees/{uid}/groups'
+GROUP_PATTERN = 'cn={role_name},ou=Roles,ou=aws-[a-z]*-{account_id},ou=AWS,ou=apps,dc=zalando,dc=net'
 
 
 def get_credentials(account_id: str, role_name: str):
-    pass
+    uid = connexion.request.user
+    realm = connexion.request.token_info['realm']
+    if realm != '/employees':
+        return connexion.problem(403, 'Forbidden', 'You are not authorized to use this service')
+    token = connexion.request.token_info['access_token']
+    response = requests.get(GROUPS_URL.format(uid=uid), headers={'Authorization': 'Bearer {}'.format(token)})
+    response.raise_for_status()
+    groups = response.json()
+    allowed = False
+    for group in groups:
+        if re.match(GROUP_PATTERN.format(role_name=role_name, account_id=account_id), group['dn']):
+            allowed = True
+            break
+    if not allowed:
+        return connexion.problem(403, 'Forbidden', 'Access to requested AWS account/role was denied')
+    return allowed
+
 
 logging.basicConfig(level=logging.INFO)
 app = connexion.App(__name__)
