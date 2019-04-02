472d2b785e1e695a8752b4afe4d9e5d89be75236
Rodrigo Reis <rodrigo.gargravarr@gmail.com>
2019-04-02 17:05:56 +0200

    Added url and tokenSource
    Signed-off-by: Rodrigo Reis <rodrigo.gargravarr@gmail.com>

--- audittrail.py
+++ audittrail.py
@@ -3,4 +3,8 @@ REQUEST_AWS_CREDENTIALS_VERSION = '1.0'
 REQUEST_AWS_CREDENTIALS_EVENT = 'request-aws-credentials'
 
 class AuditTrail:
-    pass
+    """Defines a client for sending events to AuditTrail"""
+
+    def __init__(self, url, tokenSource):
+        self.url = url
+        self.tokenSource = tokenSource
