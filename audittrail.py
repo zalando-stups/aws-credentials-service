64bb4febc48c39240e1c3b34eb38fdddea266ca7
Rodrigo Reis <rodrigo.gargravarr@gmail.com>
2019-04-16 18:31:56 +0200

    Started function to send audit events
    Signed-off-by: Rodrigo Reis <rodrigo.gargravarr@gmail.com>

--- audittrail.py
+++ audittrail.py
@@ -1,10 +1,12 @@
-AUDIT_NAMESPACE = 'cloud.zalando.com'
-REQUEST_AWS_CREDENTIALS_VERSION = '1.0'
-REQUEST_AWS_CREDENTIALS_EVENT = 'request-aws-credentials'
-
 class AuditTrail:
     """Defines a client for sending events to AuditTrail"""
+
+    namespace = 'cloud.zalando.com'
+    event = 'request-aws-credentials'
+    version = '1.0'
 
     def __init__(self, url, tokenSource):
         self.url = url
         self.tokenSource = tokenSource
+
+    def send(context, event)
