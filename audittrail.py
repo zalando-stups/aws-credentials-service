fd08550992668e163a9999e75e359073890819a4
Rodrigo Reis <rodrigo.reis@zalando.de>
2019-04-17 09:22:05 +0200

    Initial code for request_aws_credentials
    Signed-off-by: Rodrigo Reis <rodrigo.reis@zalando.de>

--- audittrail.py
+++ audittrail.py
@@ -2,11 +2,21 @@ class AuditTrail:
     """Defines a client for sending events to AuditTrail"""
 
     namespace = 'cloud.zalando.com'
-    event = 'request-aws-credentials'
+    event_name = 'request-aws-credentials'
     version = '1.0'
 
     def __init__(self, url, tokenSource):
         self.url = url
         self.tokenSource = tokenSource
 
-    def send(context, event)
+    def send(context, event):
+        pass
+
+    def request_aws_credentials(user, account):
+        event = {"event_type": { "namespace": namespace,
+                                 "name": event_name,
+                                 "varsion": version,
+                               }
+                }
+
+        return event
