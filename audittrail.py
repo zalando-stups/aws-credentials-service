a156f7fefb12ed8b30e3f63b59e29e01480e0b97
Rodrigo Reis <rodrigo.gargravarr@gmail.com>
2019-04-17 13:52:03 +0200

    Finished event structure
    Signed-off-by: Rodrigo Reis <rodrigo.gargravarr@gmail.com>

--- audittrail.py
+++ audittrail.py
@@ -1,9 +1,13 @@
+import datetime
+
 class AuditTrail:
     """Defines a client for sending events to AuditTrail"""
 
-    namespace = 'cloud.zalando.com'
-    event_name = 'request-aws-credentials'
-    version = '1.0'
+    event_type = {
+        "namespace": "cloud.zalando.com",
+        "name": "request-aws-credentials",
+        "version": "1.0",
+    }
 
     def __init__(self, url, tokenSource):
         self.url = url
@@ -12,11 +16,22 @@ class AuditTrail:
     def send(context, event):
         pass
 
-    def request_aws_credentials(user, account):
-        event = {"event_type": { "namespace": namespace,
-                                 "name": event_name,
-                                 "varsion": version,
-                               }
-                }
+    def request_aws_credentials(self, user, realm, account_id, role):
+        event = {
+            "event_type": self.event_type,
+            "triggered_at": datetime.datetime.utcnow(),
+            "triggered_by": {
+                "type": "USER",
+                "id": user,
+                "additional": {
+                    "realm": realm,
+                },
+            },
+            "payload": {
+                "user": user,
+                "account_id": account_id,
+                "role": role,
+            }
+        }
 
         return event
